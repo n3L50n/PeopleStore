c4787d3e4532dc33d795f53ca0282d3873b799db
n0de_coyote <nelsonandrewwells@gmail.com>
2017-04-07 14:59:50 -0700

    Initial commit with some uri constants in a Contract class.

---
+++ README.md
@@ -0,0 +1,1 @@
+Placed is an inventory tracking app to allow users to store, track, order, and manage the status of items. 
---
+++ app/src/main/java/com/node_coyote/placed/dataPackage/PlacedContract.java
@@ -0,0 +1,44 @@
+package com.node_coyote.placed.dataPackage;
+
+/**
+ * Created by node_coyote on 4/7/17.
+ */
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Contract for the inventory tracking app Placed
+ */
+public final class PlacedContract  {
+
+    // Empty constructor. Don't instantiate the contract class
+    public PlacedContract(){}
+
+    /**
+     *  Content Authority for content provider
+     */
+    public static final String CONTENT_AUTHORITY = "com.node_coyote.placed";
+
+    /**
+     * Base uri is created for apps to contact the content provider
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Append path to base content uri to dig into inventory table
+     */
+    public static final String PATH_INVENTORY = "inventory";
+
+    /**
+     * This inner class holds constant values for the inventory database table
+     */
+    public static final class PlacedEntry implements BaseColumns {
+
+        /** content uri to access inventory data in the provider **/
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
+        
+
+    }
+}
